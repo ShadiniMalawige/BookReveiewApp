2cc7d4038e6b0da2940324250fb55a49c3fae9a6
Shadini Nirasha <146790732+ShadiniMalawige@users.noreply.github.com>
2025-01-20 00:24:11 +0530

    frontend: review cards page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,213 @@
 import "./App.css";
+import Header from "./components/header";
+import ReviewCard from "./components/reviewCard";
+import React, { useCallback, useState, useEffect } from "react";
+import { Button, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import ReviewForm from "./components/reviewForm";
 
 function App() {
+  const [reviews, setReviews] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenEdit, setIsOpenEdit] = useState(false);
+  const [state, setState] = useState({});
+  const [submitLoading, setSubmitLoading] = useState(false);
+
+  function open() {
+    setIsOpen(true);
+  }
+
+  function close() {
+    setState({});
+    setIsOpen(false);
+  }
+  const successToast = useCallback((message) => {
+    toast.success(message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }, []);
+  const errorToast = useCallback((message) => {
+    toast.error(message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }, []);
+
+  // Fetch reviews from backend
+  const fetchReviews = useCallback(async () => {
+    try {
+      const res = await axios.get("http://localhost:4000/reviews/get");
+      setReviews(res.data);
+    } catch (error) {
+      console.error("Error fetching reviews:", error);
+      errorToast("Failed to load reviews.");
+    }
+  }, [errorToast]);
+
+  // Handle form submission
+  const handleSubmit = useCallback(async () => {
+    try {
+      setSubmitLoading(true);
+      // Check for valid inputs
+      if (
+        state?.bookTitle?.trim() === "" ||
+        state?.author?.trim() === "" ||
+        state?.reviewText?.trim() === "" ||
+        state?.rating === 0
+      ) {
+        alert("Please fill out all fields and select a rating.");
+        return;
+      }
+
+      const res = await axios.post("http://localhost:4000/reviews/add", {
+        ...state,
+      });
+
+      successToast(res?.data?.message || "Review Added Successfully!");
+      close();
+      setState({});
+      fetchReviews();
+    } catch (error) {
+      errorToast("Failed to submit review.");
+    } finally {
+      setSubmitLoading(false);
+    }
+  }, [errorToast, fetchReviews, state, successToast]);
+
+  const handleEdit = useCallback(async () => {
+    try {
+      setSubmitLoading(true);
+      // Check for valid inputs
+      if (
+        state?.bookTitle?.trim() === "" ||
+        state?.author?.trim() === "" ||
+        state?.reviewText?.trim() === "" ||
+        state?.rating === 0
+      ) {
+        alert("Please fill out all fields and select a rating.");
+        return;
+      }
+
+      const res = await axios.put(
+        `http://localhost:4000/reviews/update/${state?._id}`,
+        {
+          ...state,
+        }
+      );
+
+      successToast(res?.data?.message || "Review Updated Successfully!");
+      setIsOpenEdit(false);
+      setState({});
+      fetchReviews();
+    } catch (error) {
+      errorToast("Failed to update review.");
+    } finally {
+      setSubmitLoading(false);
+    }
+  }, [errorToast, fetchReviews, state, successToast]);
+
+  // Fetch reviews on component mount
+  useEffect(() => {
+    fetchReviews();
+  }, []);
+
   return (
     <div className="">
-      <h1>Hello World</h1>
+      <Header />
+      <div className="max-w-7xl mx-auto">
+        <div className="relative min-h-screen bg-white py-12 px-2 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between mb-8">
+            <h1 className="text-3xl font-bold text-gray-900 text-center flex-grow text-center">
+              Book Reviews
+            </h1>
+            <button
+              className="ml-auto px-4 py-2 bg-black text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              onClick={open}
+            >
+              Add Review
+            </button>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+            {reviews.map((review) => (
+              <ReviewCard
+                key={review._id}
+                review={review}
+                onClick={() => {
+                  setIsOpenEdit(true);
+                  setState(review);
+                }}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <Dialog
+        open={isOpen}
+        as="div"
+        className="relative z-10 focus:outline-none"
+        onClose={close}
+      >
+        <div className="fixed inset-0 bg-black/50 transition-opacity" />
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <DialogPanel
+              transition
+              className="w-full max-w-2xl rounded-xl bg-white/100 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+            >
+              <ReviewForm
+                setState={setState}
+                state={state}
+                onSubmit={handleSubmit}
+                submitLoading={submitLoading}
+              />
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
+      <Dialog
+        open={isOpenEdit}
+        as="div"
+        className="relative z-10 focus:outline-none"
+        onClose={() => setIsOpenEdit(false)}
+      >
+        <div className="fixed inset-0 bg-black/50 transition-opacity" />
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <DialogPanel
+              transition
+              className="w-full max-w-2xl rounded-xl bg-white/100 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+            >
+              <ReviewForm
+                setState={setState}
+                state={state}
+                onSubmit={handleEdit}
+                mode="edit"
+                submitLoading={submitLoading}
+                onClose={() => {
+                  fetchReviews();
+                  setIsOpenEdit(false);
+                }}
+              />
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
+      <ToastContainer />
     </div>
   );
 }
